e5d2902da4b7cae4b7c42e3339841335f9060bc1
Katrina Theodosopoulos <katrinaelaine6@gmail.com>
2014-06-16 09:41:31 -0500

    Postgres works. Setup Devise.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wigglez::Application.config.secret_key_base = '32fe622e3baeb9d3f3e47ece847627a97324ba98446765fc266cbfb896eeec65d76db84a8170b396c1661687c6f2815e4a5052c53983d990a29f68bb32c871dc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Wigglez::Application.config.session_store :cookie_store, key: '_wigglez_session'
