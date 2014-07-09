25dbaff20789ea6ad5fb362a7f8265c52cc76a8d
Katrina Theodosopoulos <katrinaelaine6@gmail.com>
2014-07-09 15:56:16 -0500

    Add line to config file to have Rails serve assets.

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,8 @@ module Wigglez
     config.eager_load = false
 
     config.active_record.migration_error = :page_load
+
+    config.serve_static_assets = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
